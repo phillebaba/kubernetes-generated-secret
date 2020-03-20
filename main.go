330e256fe6ae8744b54524d4f625d20b3ee3adbf
Philip Laine <philip.laine@gmail.com>
2020-03-20 14:48:33 +0100

    Add debug flag to enable/disable debug logging

--- main.go
+++ main.go
@@ -27,15 +27,16 @@ func init() {
 }
 
 func main() {
+	var debug bool
 	var metricsAddr string
 	var enableLeaderElection bool
+	flag.BoolVar(&debug, "debug", false, "Enables debug logging if true.")
 	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
-		"Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
+	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false, "Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
 	flag.Parse()
 
 	ctrl.SetLogger(zap.New(func(o *zap.Options) {
-		o.Development = true
+		o.Development = debug
 	}))
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
